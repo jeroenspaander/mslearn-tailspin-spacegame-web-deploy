9a90f927ad75984912ca10e2325c9261dd07bbc2
Jeroen Spaander <jeroen.spaander@edsp.nl>
2022-05-05 15:52:14 +0200

    Feature 1 added to Program.cs

--- Tailspin.SpaceGame.Web/Program.cs
+++ Tailspin.SpaceGame.Web/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
+// Created change due to: https://docs.microsoft.com/en-us/learn/modules/manage-git-branches-workflows/4-explore-git-branch-model-for-continuous-delivery
 namespace TailSpin.SpaceGame.Web
 {
     public class Program
